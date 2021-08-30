860f4c609bb58d50b5265457bb0067fb5ada79ba
Matheus Silva <matheus.silva@jbq.global>
2021-08-30 08:15:37 -0300

    refactor: rename to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Reset } from 'styled-reset';
 import { GlobalStyle } from './styles/global';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Reset />
       <GlobalStyle />
-      <h1>Olá Mundo!</h1>
+      <Footer />
     </>
   );
 }
