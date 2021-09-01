185780e0f4284db9c4836caf65e89f80b510ae37
Matheus Silva <matheus.silva@jbq.global>
2021-09-01 16:10:54 -0300

    feat(app): import hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Reset } from 'styled-reset';
 import { GlobalStyle } from './styles/global';
 import Footer from './components/Footer';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <>
       <Reset />
       <GlobalStyle />
+      <Hero />
       <Footer />
     </>
   );
