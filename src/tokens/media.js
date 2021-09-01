c79c7cdffeea4f056046484e10cdf12d49bb4f37
Matheus Silva <matheus.silva@jbq.global>
2021-09-01 16:34:12 -0300

    feat(media): import new breakpoints to media

--- src/tokens/media.js
+++ src/tokens/media.js
@@ -6,6 +6,8 @@ const media = {
   md: `@media (min-width: ${breakpoints.md})`,
   lg: `@media (min-width: ${breakpoints.lg})`,
   xl: `@media (min-width: ${breakpoints.xl})`,
+  xxl: `@media (min-width: ${breakpoints.xxl})`,
+  xxxl: `@media (min-width: ${breakpoints.xxxl})`,
 };
 
 export default media;
