0703d7af174acdaa003af6f6e60cdf5be7b17f79
Matheus Silva <matheus.silva@jbq.global>
2021-09-06 06:46:13 -0300

    feat(products): move PageNumbers to last component

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -42,6 +42,19 @@ const Products = () => {
 
   return (
     <>
+      <Container>
+        {currentItems.map((item) => (
+          <Product key={item.name}>
+            <img
+              src="https://secure-static.arezzo.com.br/medias/sys_master/arezzo/arezzo/h3d/hdf/h00/h00/9622502703134/1167900010006U-01-BASEIMAGE-Lores.jpg"
+              alt=""
+            />
+            <p>{item.name}</p>
+            <strong>R${item.price}</strong>
+          </Product>
+        ))}
+      </Container>
+
       <PageNumbers>
         <ul>
           <li
@@ -76,18 +89,6 @@ const Products = () => {
           </li>
         </ul>
       </PageNumbers>
-      <Container>
-        {currentItems.map((item) => (
-          <Product key={item.name}>
-            <img
-              src="https://secure-static.arezzo.com.br/medias/sys_master/arezzo/arezzo/h3d/hdf/h00/h00/9622502703134/1167900010006U-01-BASEIMAGE-Lores.jpg"
-              alt=""
-            />
-            <p>{item.name}</p>
-            <strong>R${item.price}</strong>
-          </Product>
-        ))}
-      </Container>
     </>
   );
 };
