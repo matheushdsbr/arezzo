c2d35398b9b9a63b59b59ae4cba5d85dd85c0e60
Matheus Silva <matheus.silva@jbq.global>
2021-09-09 13:39:42 -0300

    feat(hero): add and config splide to carrousel

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,8 +1,29 @@
+import { Splide, SplideSlide } from '@splidejs/react-splide';
 import { Container, BannerOne } from './styles';
 
 const Hero = () => (
   <Container>
-    <BannerOne />
+    <Splide
+      options={{
+        autoplay: true,
+        interval: 5000,
+        pauseOnHover: false,
+        arrows: false,
+        rewind: true,
+      }}
+    >
+      <SplideSlide>
+        <BannerOne banner={1} />
+      </SplideSlide>
+
+      <SplideSlide>
+        <BannerOne banner={2} />
+      </SplideSlide>
+
+      <SplideSlide>
+        <BannerOne banner={3} />
+      </SplideSlide>
+    </Splide>
   </Container>
 );
 
