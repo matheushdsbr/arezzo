f7b4daff1e475758dc13b91e2f1fd580a3f901a7
Matheus Silva <matheus.silva@jbq.global>
2021-09-09 13:41:06 -0300

    style(hero): import all banners, add styles to splide pagination and page

--- src/components/Hero/styles.js
+++ src/components/Hero/styles.js
@@ -1,17 +1,41 @@
 import styled from 'styled-components';
-import { media } from '../../tokens';
+import { media, colors } from '../../tokens';
 import Banner1 from '../../assets/banner1.jpg';
+import Banner2 from '../../assets/banner2.jpg';
+import Banner3 from '../../assets/banner3.jpg';
 
 export const Container = styled.div`
   margin-top: 78px;
-  margin-bottom: 60px;
+  margin-bottom: 100px;
+
+  .splide__pagination {
+    bottom: -30px;
+
+    &__page {
+      background: ${colors.dotsCarrousel.dots};
+
+      &.is-active {
+        background: ${colors.dotsCarrousel.active};
+      }
+    }
+  }
 `;
 
 export const BannerOne = styled.div`
-  background-image: url(${Banner1});
+  background-image: ${({ banner }) =>
+    banner === 1
+      ? `url(${Banner1})`
+      : banner === 2
+      ? `url(${Banner2})`
+      : `url(${Banner3})`};
   background-size: cover;
   background-repeat: no-repeat;
-  background-position: right 20% bottom;
+  background-position: ${({ banner }) =>
+    banner === 1
+      ? `right 20% bottom`
+      : banner === 2
+      ? `left 20% bottom`
+      : `right 12% bottom`};
   max-width: 1440px;
   height: 575px;
 
